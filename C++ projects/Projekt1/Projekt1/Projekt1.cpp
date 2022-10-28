3f2c4db11169ac3029db63c22f4183c72d5e13e3
StanislawKopec <91846304+StanislawKopec@users.noreply.github.com>
2022-10-28 10:02:14 +0200

    projekty studia

---
+++ C++ projects/Projekt1/Projekt1/Projekt1.cpp
@@ -0,0 +1,293 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class Vehicle {
+public:
+	virtual void GenerujRaport() = 0;
+	virtual string GetClass() = 0;
+	virtual string GetParameters() = 0;
+	virtual string GetNumbers() = 0;
+};
+
+class Truck : public Vehicle {
+	string Clas;
+	string Numbers;
+	int Tonnage;
+	string License;
+public:
+	Truck(string clas, string numbers, int tonnage, string license) {
+		Clas = clas;
+		Numbers = numbers;
+		Tonnage = tonnage;
+		License = license;
+	}
+	void Wypisz() {
+		cout << Numbers << endl;
+		cout << Tonnage << endl;
+		cout << License << endl;
+	}
+	string GetNumbers() {
+		return Numbers;
+	}
+	int getTonnage() {
+		return Tonnage;
+	}
+	string getLicense() {
+		return License;
+	}
+	virtual string GetParameters() {
+		stringstream ss;
+		string s;
+		ss << Clas << " " << Numbers << " " << Tonnage << " " << License;
+		getline(ss, s);
+		s = ss.str();
+		return s;
+	}
+	virtual string GetClass() {
+		return "Truck";
+	}
+	void GenerujRaport() {
+		cout << "Klasa: " << Clas << endl;
+		cout << "Pojazd: " << Numbers << endl;
+		cout << "Prawo jazdy: " << License << endl;
+		cout << "Pojemnosc: " << Tonnage << endl;
+	}
+
+	~Truck() {
+
+	}
+};
+
+class PassengerVehicle : public Vehicle {
+	string Clas;
+	string Numbers;
+	string License;
+	int PassengerLimit;
+public:
+	PassengerVehicle(string clas, string numbers, string license, int passengerLimit) {
+		Clas = clas;
+		Numbers = numbers;
+		License = license;
+		PassengerLimit = passengerLimit;
+	}
+	void Wypisz() {
+		cout << Numbers << endl;
+		cout << License << endl;
+		cout << PassengerLimit << endl;
+	}
+	string GetNumbers() {
+		return Numbers;
+	}
+	string getLicense() {
+		return License;
+	}
+	int getPassengerLimit() {
+		return PassengerLimit;
+	}
+	virtual string GetClass() {
+		return "PassengerVehicle";
+	}
+	virtual string GetParameters() {
+		stringstream ss;
+		string s;
+		ss << Clas << " " << Numbers << " " << License << " " << PassengerLimit;
+		getline(ss, s);
+		s = ss.str();
+		return s;
+	}
+	void GenerujRaport() {
+		cout << "Klasa: " << Clas << endl;
+		cout << "Pojazd: " << Numbers << endl;
+		cout << "Prawo jazdy: " << License << endl;
+		cout << "Liczba wolnych miejsc: " << PassengerLimit << endl;
+	}
+	~PassengerVehicle() {
+
+	}
+};
+
+
+int main() {
+	int x = 0;
+	string klasa;
+	string rejestracja;
+	int pojemnosc;
+	string licencja;
+	vector <unique_ptr<Vehicle>> vehicles;
+	string pojazd;
+
+	fstream newfile;
+
+	newfile.open("data.txt", ios::in);
+	if (newfile.is_open()) {
+		string tp;
+		while (getline(newfile, tp)) {
+			stringstream S;
+			string word;
+			string word1;
+			S << tp;
+			S >> word1 >> word;
+			string buffer;
+			int i = 0;
+			if (word == "Truck") {
+				string klasa;
+				string rejestracja;
+				double pojemnosc;
+				string licenjca;
+				if (S >> klasa >> rejestracja >> pojemnosc >> licencja) {
+					S >> klasa >> rejestracja >> pojemnosc >> licencja;
+				}
+				else {
+					cout << "Bledne dane" << endl;
+				}
+
+				Truck c(klasa, rejestracja, pojemnosc, licencja);
+				vehicles.push_back(make_unique<Truck>(c));
+			}
+			else if (word == "PassengerVehicle") {
+				string klasa;
+				string rejestracja;
+				string licencja;
+				int miejsca;
+				while (S >> buffer) {
+					if (S >> klasa >> rejestracja >> licencja >> miejsca) {
+						S >> klasa >> rejestracja >> licencja >> miejsca;
+					}
+					else {
+						cout << "Bledne dane" << endl;
+					}
+				}
+				PassengerVehicle c(klasa, rejestracja, licencja, miejsca);
+				vehicles.push_back(make_unique<PassengerVehicle>(c));
+			}
+		}
+		newfile.close();
+	}
+	else {
+		cout << "Nie mozna otowrzyc pliku" << endl;
+	}
+
+	while (x != 1) {
+		cout << "Wybierz akcje:" << endl << "1) Koniec " << endl << "2) Dodaj pojazd" << endl << "3) Usun pojazd" << endl << "4) Generuj raport" << endl;
+		cin >> x;
+		switch (x) {
+		case 1:
+			cout << "Koniec" << endl;
+			break;
+		case 2:
+			cout << "Podaj kategorie pojazdu" << endl;
+			cin >> klasa;
+			if (klasa == "Truck") {
+				string klasa = "Truck";
+				cout << "rejestracja" << endl;
+				cin >> rejestracja;
+				cout << "pojemnosc" << endl;
+				cin >> pojemnosc;
+				while (!cin.good()) {
+					cin.clear();
+					std::cin.ignore();
+					cin >> pojemnosc;
+				}
+				cout << "licencja" << endl;
+				cin >> licencja;
+				Truck c(klasa, rejestracja, pojemnosc, licencja);
+				vehicles.push_back(make_unique<Truck>(c));
+			}
+
+			else if (klasa == "PassengerVehicle") {
+				int miejsca;
+				string klasa = "PassengerVehicle";
+				cout << "rejestracja" << endl;
+				cin >> rejestracja;
+				cout << "ilosc miejsc" << endl;
+				cin >> miejsca;
+				while (!cin.good()) {
+					cin.clear();
+					std::cin.ignore();
+					cin >> pojemnosc;
+				}
+				cout << "licencja" << endl;
+				cin >> licencja;
+				PassengerVehicle c(klasa, rejestracja, licencja, miejsca);
+				vehicles.push_back(make_unique<PassengerVehicle>(c));
+			}
+			else {
+				cout << "Nie ma takiej kategorii" << endl;
+				cout << "Wpisz jedną z mozliwych: Truck, PassengerVehicle" << endl;
+			}
+
+			newfile.open("data.txt", ios::out | ios::app);
+			if (newfile.is_open())
+			{
+				for (int i = 0; vehicles.size() > i; ++i) {
+					Vehicle* wsk;
+					wsk = vehicles[i].get();
+					if (wsk->GetClass() == "Truck") {
+						newfile << wsk->GetParameters() << endl;
+					}
+					else if (wsk->GetClass() == "PassengerVehicle") {
+						newfile << wsk->GetParameters() << endl;
+					}
+				}
+				newfile.close();
+			}
+			else {
+				cout << "Nie mozna otowrzyc pliku" << endl;
+			}
+			break;
+		case 3:
+			cout << "Wybierz pojazd do usuniecia, po numerze rejestracji" << endl;
+			cin >> pojazd;
+			for (int i = 0; vehicles.size() > i; ++i) {
+				Vehicle* wsk;
+				wsk = vehicles[i].get();
+				if (wsk->GetNumbers() == rejestracja) {
+					vehicles.erase(vehicles.begin() + i);
+					break;
+				}
+			}
+			cout << "Nie ma takiego pojazdu" << endl;
+
+			newfile.open("data.txt", ios::out);
+			if (newfile.is_open())
+			{
+				for (int i = 0; vehicles.size() > i; ++i) {
+					Vehicle* wsk;
+					wsk = vehicles[i].get();
+					if (wsk->GetClass() == "Truck") {
+						newfile << wsk->GetParameters();
+					}
+					else if (wsk->GetClass() == "PassengerVehicle") {
+						newfile << wsk->GetParameters();
+					}
+				}
+				newfile.close();
+			}
+			else {
+				cout << "Nie mozna otowrzyc pliku" << endl;
+			}
+
+			break;
+		case 4:
+			string rejestracja;
+			cin >> rejestracja;
+			for (int i = 0; vehicles.size() > i; ++i) {
+				Vehicle* wsk;
+				wsk = vehicles[i].get();
+				if (wsk->GetNumbers() == rejestracja) {
+					wsk->GenerujRaport();
+					break;
+				}
+			}
+			cout << "Nie ma takiego pojazdu" << endl;
+			break;
+		}
+	}
+}
---
+++ C++ projects/Projekt2/Projekt2/Projekt2.cpp
@@ -0,0 +1,235 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+void write_to_file(map<string, vector<map<string, float>>> result, string file_name) {
+    ofstream o;
+    o.open(file_name);
+
+    for (const auto& pair : result) {
+        o << pair.first << endl;
+
+        for (const auto& record : pair.second) {
+
+            for (const auto& field : record) {
+                o << field.second << " ";
+            }
+            o << endl;
+
+        }
+        o << endl;
+    }
+
+    o.close();
+}
+
+bool is_number(const string& str)
+{
+    for (char const& c : str) {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
+
+string read_till_term(string raw, size_t& start) {
+    string buffer = "";
+    size_t i = start;
+    for (i = start; i < raw.size(); i++) {
+        if (raw[i] == ' ' || raw[i] == '%') {
+            start = i + 1;
+            return buffer;
+        }
+        buffer += raw[i];
+    }
+
+    start = i;
+    return buffer;
+
+}
+
+vector<map<string, float>> parse_data(string file_name) {
+    ifstream t(file_name);
+    string line;
+
+    std::getline(t, line);
+
+    vector<string> fields;
+    string buffer;
+    size_t i = 0;
+
+    while ((buffer = read_till_term(line, i)) != "") {
+        fields.push_back(buffer);
+    }
+
+    vector<map<string, float>> data;
+    while (std::getline(t, line)) {
+
+        map<string, float> curr_row;
+        string buffer;
+        int field_i = 0;
+        for (size_t i = 0; i < line.size(); i++) {
+            if (line[i] == ' ') {
+                curr_row[fields[field_i]] = stof(buffer);
+                buffer = "";
+                field_i += 1;
+                continue;
+            }
+            buffer += line[i];
+        }
+        curr_row[fields[field_i]] = stof(buffer);
+
+        data.push_back(curr_row);
+    }
+
+    t.close();
+
+    return data;
+}
+
+class TreeNode {
+public:
+    int index;
+    string attr;
+    int oper;
+    float value;
+
+    string on_false = "";
+    string on_true = "";
+};
+
+enum OperTypes {
+    GREATER,
+    SMALLER,
+};
+
+enum TreeFields {
+    INDEX,
+    ATTR,
+    OPER,
+    VALUE,
+    INDEX_FALSE,
+    INDEX_TRUE,
+};
+
+map<int, TreeNode> parse_tree(string file_name) {
+    ifstream t(file_name);
+    string line;
+    map<int, TreeNode> nodes;
+
+    while (std::getline(t, line)) {
+        string buffer;
+        size_t i = 0;
+        size_t iter = 0;
+
+        TreeNode tn;
+
+        while (i < line.length()) {
+            buffer = read_till_term(line, i);
+
+            switch (iter) {
+            case INDEX:
+                tn.index = stoi(buffer);
+                break;
+            case ATTR:
+                tn.attr = buffer;
+                break;
+            case OPER:
+                tn.oper = buffer == ">" ? GREATER : SMALLER;
+                break;
+            case VALUE:
+                tn.value = stof(buffer);
+                break;
+            case INDEX_FALSE:
+                tn.on_false = buffer;
+                break;
+            case INDEX_TRUE:
+                tn.on_true = buffer;
+                break;
+            default:
+                cout << "Undefined field" << endl;
+            }
+
+            iter++;
+        }
+        nodes[tn.index] = tn;
+    }
+
+    return nodes;
+}
+
+string evaluate(map<string, float>& record, const map<int, TreeNode>& tn_map) {
+
+    auto curr_node = tn_map.at(0);
+    string next;
+
+    while (true) {
+        float record_value = record[curr_node.attr];
+        int oper = curr_node.oper;
+
+        if (oper == SMALLER) {
+            next = record_value > curr_node.value ? curr_node.on_true : curr_node.on_false;
+        }
+        else {
+            next = record_value < curr_node.value ? curr_node.on_true : curr_node.on_false;
+        }
+
+        if (is_number(next)) {
+            curr_node = tn_map.at(stoi(next));
+            continue;
+        }
+        else {
+            return next;
+        }
+    }
+}
+
+
+
+int main(int argc, char** argv)
+{
+
+
+    string i_file = "data.txt";
+    string o_file = "output.txt";
+    string t_file = "tree.txt";
+
+    string last = "";
+    for (size_t i = 1; i < argc; i++) {
+        if (last == "-i") {
+            i_file = argv[i];
+        }
+        else if (last == "-o") {
+            o_file = argv[i];
+        }
+        else if (last == "-t") {
+            t_file = argv[i];
+        }
+        last = argv[i];
+    }
+
+
+    vector<map<string, float>> data;
+    map<int, TreeNode> tn_map;
+
+    data = parse_data(i_file);
+    tn_map = parse_tree(t_file);
+
+    map <string, vector<map<string, float>>> ordered;
+
+    for (auto& record : data) {
+        string result = evaluate(record, tn_map);
+        ordered[result].push_back(record);
+    }
+
+    write_to_file(ordered, o_file);
+
+    return 0;
+}
